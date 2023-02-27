b847bd30a2b69fdbbabb03af9081082da7d735c0
Helen Scott <helengracescott@gmail.com>
2023-02-27 09:49:33 -0500

    Add a complex media from Ilija

--- alt_core/comets_simulation.py
+++ alt_core/comets_simulation.py
@@ -10,17 +10,39 @@ import pickle
 # Create empty 1x1 layout
 test_tube = c.layout()
 
-# Add 11mM glucose
-test_tube.set_specific_metabolite('glc__D_e', 0.011)
-
-# Add plenty of oxygen
-test_tube.set_specific_metabolite('o2_e', 1000)
-
-# Add the rest of nutrients unlimited (ammonia, phosphate, water and protons)
-test_tube.set_specific_metabolite('nh4_e',1000);
-test_tube.set_specific_metabolite('pi_e',1000);
-test_tube.set_specific_metabolite('h2o_e',1000);
-test_tube.set_specific_metabolite('h_e',1000);
+# Set a rich media (copied from one of Ilija's notebooks)
+test_tube.set_specific_metabolite('4abz_e',5.47e-8)
+test_tube.set_specific_metabolite('btn_e',1.31e-8)
+test_tube.set_specific_metabolite('ca2_e',1000)
+test_tube.set_specific_metabolite('cl_e',1000)
+test_tube.set_specific_metabolite('cobalt2_e',1000)
+test_tube.set_specific_metabolite('cu2_e',1000)
+test_tube.set_specific_metabolite('fe2_e',1000)
+test_tube.set_specific_metabolite('fe3_e',1000) 
+test_tube.set_specific_metabolite('fol_e',7.3e-9)
+test_tube.set_specific_metabolite('glc__D_e',0.00075)
+test_tube.set_specific_metabolite('k_e',1000)
+test_tube.set_specific_metabolite('lac__D_e',0)
+test_tube.set_specific_metabolite('lac__L_e',0)
+test_tube.set_specific_metabolite('mg2_e',1000)
+test_tube.set_specific_metabolite('mn2_e',1000)
+test_tube.set_specific_metabolite('mobd_e',1000)
+test_tube.set_specific_metabolite('na_e',1000)
+#test_tube.set_specific_metabolite('nac_e',6.09e-8)
+test_tube.set_specific_metabolite('nac_e',0.0002)
+#test_tube.set_specific_metabolite('nh4_e',0.0007)
+test_tube.set_specific_metabolite('nh4_e',0.00012)
+test_tube.set_specific_metabolite('o2_e',1000)
+test_tube.set_specific_metabolite('pi_e',0.000645)
+#test_tube.set_specific_metabolite('pi_e',1000)
+test_tube.set_specific_metabolite('pnto__R_e',3.4e-8)
+#test_tube.set_specific_metabolite('pnto__R_e',0)
+test_tube.set_specific_metabolite('pydxn_e',8.865e-9)
+test_tube.set_specific_metabolite('ribflv_e',2e-8)
+test_tube.set_specific_metabolite('sel_e',1000)
+test_tube.set_specific_metabolite('so3_e',1000)
+test_tube.set_specific_metabolite('so4_e',1000)
+test_tube.set_specific_metabolite('zn2_e',1000)
 
 # create the model using CobraPy functionality
 alt_cobra = cobra.io.read_sbml_model("../../GEM-repos/mit1002-core-model/core_314275.5_GP.SBML/core_314275.5_GP.xml")
@@ -33,7 +55,7 @@ alt_cobra.objective = "bio1_biomass"
 alt = c.model(alt_cobra)
 
 # remove the bounds from glucose import (will be set dynamically by COMETS)
-alt.change_bounds('EX_glc__D_e', -1000, 1000)
+alt.change_bounds('EX_glc__D_e', -1000, 1000) # FIXME: Do I need to change the name to be the KBase ID?
 alt.change_bounds('EX_ac_e', -1000, 1000)
 
 # set the model's initial biomass
