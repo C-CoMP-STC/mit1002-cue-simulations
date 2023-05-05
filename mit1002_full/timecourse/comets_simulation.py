767b7e714640c3a03f68704238c16c72776ee60c
Helen G Scott <helengracescott@gmail.com>
2023-05-05 15:51:24 -0400

    Run COMETS with pFBA

--- mit1002_full/timecourse/comets_simulation.py
+++ mit1002_full/timecourse/comets_simulation.py
@@ -10,18 +10,30 @@ import pickle
 # Create empty 1x1 layout
 test_tube = c.layout()
 
-# Add compounds in "Glucose minimal media from the SI file"
 # Set glucose os 0.011, everything else as 1000
-test_tube.set_specific_metabolite('glc__D_e', 0.011)
+test_tube.set_specific_metabolite('cpd00027_e0', 0.011) # D-Glucose_e0
 
-# Add plenty of oxygen
-test_tube.set_specific_metabolite('o2_e', 1000)
+# Add a limiting amount of oxygen- not sure the exact amount I should use
+test_tube.set_specific_metabolite('cpd00007_e0', 2) # O2_e0
 
-# Add the rest of nutrients unlimited (ammonia, phosphate, water and protons)
-test_tube.set_specific_metabolite('nh4_e',1000)
-test_tube.set_specific_metabolite('pi_e',1000)
-test_tube.set_specific_metabolite('h2o_e',1000)
-test_tube.set_specific_metabolite('h_e',1000)
+# Add the of the nutrients I found in the find_minimal_media notebook
+test_tube.set_specific_metabolite('cpd00058_e0', 1000); # Cu2+_e0
+test_tube.set_specific_metabolite('cpd00971_e0', 1000); # Na+_e0
+test_tube.set_specific_metabolite('cpd00063_e0', 1000); # Ca2+_e0
+test_tube.set_specific_metabolite('cpd00048_e0', 1000); # Sulfate_e0
+test_tube.set_specific_metabolite('cpd10516_e0', 1000); # fe3_e0
+test_tube.set_specific_metabolite('cpd00254_e0', 1000); # Mg_e0
+test_tube.set_specific_metabolite('cpd00009_e0', 1000); # Phosphate_e0
+test_tube.set_specific_metabolite('cpd00205_e0', 1000); # K+_e0
+test_tube.set_specific_metabolite('cpd00013_e0', 1000); # NH3_e0
+test_tube.set_specific_metabolite('cpd00099_e0', 1000); # Cl-_e0
+test_tube.set_specific_metabolite('cpd00030_e0', 1000); # Mn2+_e0
+test_tube.set_specific_metabolite('cpd00075_e0', 1000); # Nitrite_e0
+test_tube.set_specific_metabolite('cpd00001_e0', 1000); # H2O_e0
+test_tube.set_specific_metabolite('cpd00635_e0', 1000); # Cbl_e0
+test_tube.set_specific_metabolite('cpd00034_e0', 1000); # Zn2+_e0
+test_tube.set_specific_metabolite('cpd00149_e0', 1000); # Co2+_e0
+
 
 # create the model using CobraPy functionality
 alt_cobra = cobra.io.read_sbml_model("../../GEM-repos/mit1002-model/model.xml")
@@ -29,9 +41,12 @@ alt_cobra = cobra.io.read_sbml_model("../../GEM-repos/mit1002-model/model.xml")
 # use the loaded model to build a comets model
 alt = c.model(alt_cobra)
 
-# remove the bounds from glucose import (will be set dynamically by COMETS)
-alt.change_bounds('EX_glc__D_e', -1000, 1000)
-alt.change_bounds('EX_ac_e', -1000, 1000)
+# Convert the objective style so that the model uses pFBA
+alt.obj_style = 'MAX_OBJECTIVE_MIN_TOTAL'
+
+# remove the bounds from glucose import (will be set dynamically by COMETS
+alt.change_bounds('EX_cpd00027_e0', -1000, 1000)
+alt.change_bounds('EX_cpd00029_e0', -1000, 1000)
 
 # set the model's initial biomass
 alt.initial_pop = [0, 0, 5e-6]
@@ -60,6 +75,6 @@ experiment = c.comets(test_tube, sim_params)
 experiment.run()
 
 # Save the results
-with open('alt_full/results.pkl', 'wb') as f:
+with open('mit1002_full/timecourse/results.pkl', 'wb') as f:
     pickle.dump(experiment, f)
 
