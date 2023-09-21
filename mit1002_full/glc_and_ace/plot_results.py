1f02bacdbce7a4acaa1801fdbb71c3eefaeb4e5e
Helen G Scott <helengracescott@gmail.com>
2023-09-21 15:51:29 -0400

    Try to add a broken axis to the bar chart

--- mit1002_full/glc_and_ace/plot_results.py
+++ mit1002_full/glc_and_ace/plot_results.py
@@ -73,16 +73,44 @@ def plot_glc_and_ace_fluxes(model: cobra.Model, solution_fluxes: dict,
 
     # Barchart
     # Increase the figure size
-    plt.figure(figsize=(10, 15))  # Make figure large enough for reaction names
-    plt.bar(range(len(fluxes_to_plot)),
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    fig.subplots_adjust(hspace=0.05)  # adjust space between axes
+
+    # plt.figure(figsize=(10, 15))  # Make figure large enough for reaction names
+
+    # Plot the same data on both axes
+    ax1.bar(range(len(fluxes_to_plot)),
             list(fluxes_to_plot.values()),
             align='center')
+    ax2.bar(range(len(fluxes_to_plot)),
+            list(fluxes_to_plot.values()),
+            align='center')
+    
+    # Zoom in to different ranges on the different axes
+    ax1.set_ylim(-20, 10)
+    ax2.set_ylim(-135, -100)
+
+    # Hide the spines between ax1 and ax2
+    ax1.spines.bottom.set_visible(False)
+    ax2.spines.top.set_visible(False)
+    ax1.xaxis.tick_top()
+    ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+    ax2.xaxis.tick_bottom()
+
+    # Add slanted lines between the axes
+    d = .5  # proportion of vertical to horizontal extent of the slanted line
+    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+                linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+    ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+    ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+
+
     plt.xticks(range(len(fluxes_to_plot)),
             list(fluxes_to_plot.keys()))
     # Turn the x-axis labels sideways
     plt.xticks(rotation=90)
     # Add a title
-    plt.title('Glucose and Acetate Fluxes for FBA on ' + plot_title)
+    ax1.title('Glucose and Acetate Fluxes for FBA on ' + plot_title)
     # Tight layout so that the captions don't run off the page
     plt.tight_layout()
 
