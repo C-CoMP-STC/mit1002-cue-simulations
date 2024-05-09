37e5eed1f2aae5d8da666b0bf179e1f7df84c4ca
Helen G Scott <helengracescott@gmail.com>
2024-05-09 17:48:35 -0400

    Get the axis labels to fit

--- plot_styles.py
+++ plot_styles.py
@@ -22,12 +22,16 @@ def set_plot_style(g):
 def carbon_fates_bar(data):
     # Check that the column names and order are correct
     assert data.columns.to_list() == ["biomass", "organic_c", "co2"]
+
+    # Get the maximum length of the condition names
+    max_label_len = data.index.str.len().max()
+
     # Plot the stacked bar plot
     g = data.plot(kind="bar", stacked=True, color=[DARK_BLUE, LIGHT_BLUE, ORANGE])
     # Move the legend outside of the plot
     custom_labels = ["Biomass", "Organic C", "CO2"]
     lgd = plt.legend(
-        bbox_to_anchor=(0.5, -0.20),
+        bbox_to_anchor=(0.5, -0.044 * max_label_len),  # 42 was a magic number to get the legend to go below the labels, not sure how transferable this is
         loc="upper center",
         borderaxespad=0.0,
         ncol=3,
@@ -37,7 +41,7 @@ def carbon_fates_bar(data):
     for text in lgd.get_texts():
         text.set_color("gray")
     # Adjust the bottom margin
-    plt.subplots_adjust(bottom=0.2)
+    plt.subplots_adjust(bottom=0.025*max_label_len)
     # Style
     set_plot_style(g)
     # Title the plot and make it gray
