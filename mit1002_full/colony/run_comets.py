c227b5611ad470441a219abe0f83a82a3cd98dae
Helen G Scott <helengracescott@gmail.com>
2024-05-13 20:08:14 -0400

    Plot after new run of COMETS

--- mit1002_full/colony/run_comets.py
+++ mit1002_full/colony/run_comets.py
@@ -3,10 +3,11 @@
 # The simulation is for the MIT1002 model in a 9x9 grid with 11mM glucose
 # as the only carbon source
 
-import cometspy as c
-import cobra
-import pickle
 import os
+import pickle
+
+import cobra
+import cometspy as c
 
 # Set the output directory (where the results.pkl file will be saved)
 OUT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -22,22 +23,27 @@ cobra_model = cobra.io.read_sbml_model("../../GEM-repos/mit1002-model/model.xml"
 # use the loaded model to build a comets model
 comets_model = c.model(cobra_model)
 
+# Set the model's parameters
+comets_model.neutralDrift = True
+comets_model.add_neutral_drift_parameter(0.000001)
+comets_model.add_nonlinear_diffusion_parameters(0.0, 600000e-6, 1.0, 1.0, 0.00001)
+
 # remove the bounds from glucose import (will be set dynamically by COMETS)
-comets_model.change_bounds('EX_cpd00027_e0', -1000, 1000)
-comets_model.change_bounds('EX_cpd00029_e0', -1000, 1000)
+comets_model.change_bounds("EX_cpd00027_e0", -1000, 1000)
+comets_model.change_bounds("EX_cpd00029_e0", -1000, 1000)
 
 # Set bounds on O2
-comets_model.change_bounds('EX_cpd00007_e0', -20, 1000)
+comets_model.change_bounds("EX_cpd00007_e0", -20, 1000)
 
 # set the model's initial biomass
 # FIXME: I think this is overwriting, and only using the last!
-comets_model.initial_pop = [4, 4, 5E-6]
+comets_model.initial_pop = [4, 4, 5e-6]
 
 # add it to the layout
 layout.add_model(comets_model)
 
 # Add 11mM glucose
-layout.set_specific_metabolite('cpd00027_e0', 0.011)
+layout.set_specific_metabolite("cpd00027_e0", 0.011)
 
 # Add plenty of oxygen
 # FIXME: Should this be lower? to get a realistic bound on O2 flux?
@@ -64,23 +70,23 @@ layout.set_specific_metabolite("cpd00149_e0", 1000)  # Co2+_e0
 
 # Set the parameters that are different from the default
 sim_params = c.params()
-sim_params.set_param('biomassMotionStyle', 'ConvNonlin Diffusion 2D')
-sim_params.set_param('numDiffPerStep', 1)
-sim_params.set_param('growthDiffRate', 0)
-sim_params.set_param('allowCellOverlap', True)
-sim_params.set_param('toroidalWorld', False)
-sim_params.set_param('defaultVmax', 10)
-sim_params.set_param('defaultKm', 0.000015)
-sim_params.set_param('maxCycles', 3000)
-sim_params.set_param('timeStep', .1)
-sim_params.set_param('spaceWidth', 0.01)
-sim_params.set_param('maxSpaceBiomass', 10)
-sim_params.set_param('minSpaceBiomass', 1e-9)
-sim_params.set_param('writeMediaLog', True)
-sim_params.set_param('writeFluxLog', True)
-sim_params.set_param('writeBiomassLog', True)
-sim_params.set_param('BiomassLogRate', 100)
-sim_params.set_param('FluxLogRate', 100)
+sim_params.set_param("biomassMotionStyle", "ConvNonlin Diffusion 2D")
+sim_params.set_param("numDiffPerStep", 1)
+sim_params.set_param("growthDiffRate", 0)
+sim_params.set_param("allowCellOverlap", True)
+sim_params.set_param("toroidalWorld", False)
+sim_params.set_param("defaultVmax", 10)
+sim_params.set_param("defaultKm", 0.000015)
+sim_params.set_param("maxCycles", 3000)
+sim_params.set_param("timeStep", 0.1)
+sim_params.set_param("spaceWidth", 0.01)
+sim_params.set_param("maxSpaceBiomass", 10)
+sim_params.set_param("minSpaceBiomass", 1e-9)
+sim_params.set_param("writeMediaLog", True)
+sim_params.set_param("writeFluxLog", True)
+sim_params.set_param("writeBiomassLog", True)
+sim_params.set_param("BiomassLogRate", 100)
+sim_params.set_param("FluxLogRate", 100)
 
 # Create the experiment
 experiment = c.comets(layout, sim_params)
@@ -89,5 +95,5 @@ experiment = c.comets(layout, sim_params)
 experiment.run()
 
 # Save the results
-with open(os.path.join(OUT_DIR, 'results.pkl'), 'wb') as f:
+with open(os.path.join(OUT_DIR, "results.pkl"), "wb") as f:
     pickle.dump(experiment, f)
