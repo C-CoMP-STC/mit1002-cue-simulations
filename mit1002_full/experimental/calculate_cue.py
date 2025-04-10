b709ece26b01ffcb5e22535c0732a256af27fe32
Helen G Scott <helengracescott@gmail.com>
2025-04-10 13:26:12 -0400

    Outline scipt for working with Zac's data

---
+++ mit1002_full/experimental/calculate_cue.py
@@ -0,0 +1,10 @@
+import os
+
+DATA_DIR = "/Users/helenscott/Documents/PhD/Segre-lab/CUE/Zac txt data"
+OUT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# Load Zac's data
+
+# Calculate CUE/GGE/BGE
+
+# Save the results
