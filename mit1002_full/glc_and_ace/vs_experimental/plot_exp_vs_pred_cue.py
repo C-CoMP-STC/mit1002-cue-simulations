bec8e40c2cf5947ea3bab90425736097638b7fe5
Helen G Scott <helengracescott@gmail.com>
2025-04-11 13:36:16 -0400

    Add other O2 conditions

--- mit1002_full/glc_and_ace/vs_experimental/plot_exp_vs_pred_cue.py
+++ mit1002_full/glc_and_ace/vs_experimental/plot_exp_vs_pred_cue.py
@@ -20,8 +20,8 @@ if not os.path.exists(OUT_DIR):
 data = {
     "Glucose Only": [0.80],
     "Acetate Only": [0.49],
-    "Heavy Glucose Mix": [0.71],
-    "Heavy Acetate Mix": [0.62],
+    "Glucose Heavy Mix": [0.71],
+    "Acetate Heavy Mix": [0.62],
 }
 # Convert data to a pandas DataFrame
 gge_df = pd.DataFrame(data)
@@ -34,28 +34,67 @@ fba_data = pd.read_csv(
 )
 
 # Add the FBA-predicted results to the dataframe
-gge_df.loc["FBA"] = {
-    "Glucose Only": fba_data.loc["glc_medium_real_o2_fba", "gge"],
-    "Acetate Only": fba_data.loc["ace_medium_real_o2_fba", "gge"],
-    "Heavy Glucose Mix": fba_data.loc["glc_heavy_mix_medium_real_o2_fba", "gge"],
-    "Heavy Acetate Mix": fba_data.loc["ace_heavy_mix_medium_real_o2_fba", "gge"],
+gge_df.loc["FBA (O2=5)"] = {
+    "Glucose Only": fba_data.loc["Glucose Only(O2 = 5)_fba", "gge"],
+    "Acetate Only": fba_data.loc["Acetate Only(O2 = 5)_fba", "gge"],
+    "Glucose Heavy Mix": fba_data.loc["Glucose Heavy Mix(O2 = 5)_fba", "gge"],
+    "Acetate Heavy Mix": fba_data.loc["Acetate Heavy Mix(O2 = 5)_fba", "gge"],
+}
+gge_df.loc["FBA (O2=10)"] = {
+    "Glucose Only": fba_data.loc["Glucose Only(O2 = 10)_fba", "gge"],
+    "Acetate Only": fba_data.loc["Acetate Only(O2 = 10)_fba", "gge"],
+    "Glucose Heavy Mix": fba_data.loc["Glucose Heavy Mix(O2 = 10)_fba", "gge"],
+    "Acetate Heavy Mix": fba_data.loc["Acetate Heavy Mix(O2 = 10)_fba", "gge"],
+}
+gge_df.loc["FBA (O2=20)"] = {
+    "Glucose Only": fba_data.loc["Glucose Only(O2 = 20)_fba", "gge"],
+    "Acetate Only": fba_data.loc["Acetate Only(O2 = 20)_fba", "gge"],
+    "Glucose Heavy Mix": fba_data.loc["Glucose Heavy Mix(O2 = 20)_fba", "gge"],
+    "Acetate Heavy Mix": fba_data.loc["Acetate Heavy Mix(O2 = 20)_fba", "gge"],
+}
+gge_df.loc["FBA (O2=30)"] = {
+    "Glucose Only": fba_data.loc["Glucose Only(O2 = 30)_fba", "gge"],
+    "Acetate Only": fba_data.loc["Acetate Only(O2 = 30)_fba", "gge"],
+    "Glucose Heavy Mix": fba_data.loc["Glucose Heavy Mix(O2 = 30)_fba", "gge"],
+    "Acetate Heavy Mix": fba_data.loc["Acetate Heavy Mix(O2 = 30)_fba", "gge"],
+}
+gge_df.loc["FBA (O2=1000)"] = {
+    "Glucose Only": fba_data.loc["Glucose Only(O2 = 1000)_fba", "gge"],
+    "Acetate Only": fba_data.loc["Acetate Only(O2 = 1000)_fba", "gge"],
+    "Glucose Heavy Mix": fba_data.loc["Glucose Heavy Mix(O2 = 1000)_fba", "gge"],
+    "Acetate Heavy Mix": fba_data.loc["Acetate Heavy Mix(O2 = 1000)_fba", "gge"],
 }
 
 # Plot the predicted vs experimental
 # Transpose the DataFrame to make rows into columns for easier plotting
 gge_df_transposed = gge_df.T
 
-# Create a scatter plot of the two rows
-plt.scatter(gge_df_transposed["Experimental"], gge_df_transposed["FBA"], label="Model v2 (O2=20)")
+# Create a scatter plot of the two rows for each of the conditions
+plt.figure(figsize=(8, 8))
+plt.scatter(gge_df_transposed["Experimental"],
+            gge_df_transposed["FBA (O2=5)"],
+            label="Model v2 (O2=5)")
+plt.scatter(gge_df_transposed["Experimental"],
+            gge_df_transposed["FBA (O2=10)"],
+            label="Model v2 (O2=10)")
+plt.scatter(gge_df_transposed["Experimental"],
+            gge_df_transposed["FBA (O2=20)"],
+            label="Model v2 (O2=20)")
+plt.scatter(gge_df_transposed["Experimental"],
+            gge_df_transposed["FBA (O2=30)"],
+            label="Model v2 (O2=30)")
+plt.scatter(gge_df_transposed["Experimental"],
+            gge_df_transposed["FBA (O2=1000)"],
+            label="Model v2 (O2=1000)")
 
-# Add a line of best fit
-# Calculate the coefficients for the line of best fit
-m, b = np.polyfit(gge_df_transposed["Experimental"], gge_df_transposed["FBA"], 1)
-# Create the line of best fit
-x_fit = np.array([0, 1])
-y_fit = m * x_fit + b
-# Plot the line of best fit
-plt.plot(x_fit, y_fit, color="blue", label=None)
+# # Add a line of best fit
+# # Calculate the coefficients for the line of best fit
+# m, b = np.polyfit(gge_df_transposed["Experimental"], gge_df_transposed["FBA"], 1)
+# # Create the line of best fit
+# x_fit = np.array([0, 1])
+# y_fit = m * x_fit + b
+# # Plot the line of best fit
+# plt.plot(x_fit, y_fit, color="blue", label=None)
 
 # Add a 1:1 line for reference
 x = [0, 1]
