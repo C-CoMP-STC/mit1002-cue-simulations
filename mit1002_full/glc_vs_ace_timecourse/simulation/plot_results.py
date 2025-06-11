2e109b9f12246101768d72d942d0c308f89ac4a0
Helen G Scott <helengracescott@gmail.com>
2025-06-11 10:25:10 -0400

    Remove shading based on growth rate mode, plot exchange fluxes

--- mit1002_full/glc_vs_ace_timecourse/simulation/plot_results.py
+++ mit1002_full/glc_vs_ace_timecourse/simulation/plot_results.py
@@ -5,7 +5,6 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 RESULTS_DIR = os.path.join(FILE_DIR, "results")
 PLOTS_DIR = os.path.join(FILE_DIR, "plots")
 
-n_sigfigs = 3  # Number of digits to round the growth rate to
 model_id = "iHS4156"  # Model ID for the COMETS model
 biomass_id = "bio1_biomass"  # Biomass reaction ID in the model
 
@@ -23,25 +22,8 @@ for filename in os.listdir(RESULTS_DIR):
         # Take "_results.pkl" off the end of the filename to get the experiment name
         c_source_name = filename.split("_results.pkl")[0]
 
-        # Round the growth rate to n_sigfigs
-        growth_rate_per_cycle = experiment.fluxes_by_species[model_id][
-            biomass_id
-        ].round(n_sigfigs)
-
-        # Find the most common growth rate that is not zero
-        growth_rate_per_cycle = growth_rate_per_cycle[growth_rate_per_cycle != 0]
-        most_common_growth_rate = growth_rate_per_cycle.value_counts().idxmax()
-
-        # Find all cycles with the most common growth rate
-        phase_cycles = growth_rate_per_cycle[
-            growth_rate_per_cycle == most_common_growth_rate
-        ].index
-
         # Plot the biomass over time
         ax = experiment.total_biomass.plot(x="cycle")
-        # Shade the regions of growth in the phase cycles
-        for cycle in phase_cycles:
-            ax.axvspan(cycle - 0.5, cycle + 0.5, color="green", alpha=0.3)
         # Style the plot
         ax.set_ylabel("Biomass (g)")
         ax.set_title(f"MIT1002 Growth on {c_source_name}")
@@ -49,16 +31,16 @@ for filename in os.listdir(RESULTS_DIR):
 
         # Plot the growth rate over time
         ax = experiment.fluxes_by_species[model_id].plot(x="cycle", y=biomass_id)
-        # Shade the regions of growth in the phase cycles
-        for cycle in phase_cycles:
-            ax.axvspan(cycle - 0.5, cycle + 0.5, color="green", alpha=0.3)
         # Style the plot
         ax.set_ylabel("Growth Rate (1/h)")
         ax.set_title(f"MIT1002 Growth Rate on {c_source_name}")
         ax.figure.savefig(os.path.join(PLOTS_DIR, f"{c_source_name}_growth_rate.png"))
 
-        # Save the list of phase cycles to a pickle file
-        with open(
-            os.path.join(RESULTS_DIR, f"{c_source_name}_phase_cycles.pkl"), "wb"
-        ) as f:
-            pickle.dump(phase_cycles, f)
+        # Plot the glucose and acetate exchange fluxes over time
+        ax = experiment.fluxes_by_species[model_id].plot(
+            x="cycle", y=["EX_cpd00027_e0", "EX_cpd00029_e0"]
+        )
+        # Style the plot
+        ax.set_ylabel("Exchange Flux (mmol/gDW/h)")
+        ax.set_title(f"MIT1002 Exchange Fluxes on {c_source_name}")
+        ax.figure.savefig(os.path.join(PLOTS_DIR, f"{c_source_name}_exchange_fluxes.png"))
