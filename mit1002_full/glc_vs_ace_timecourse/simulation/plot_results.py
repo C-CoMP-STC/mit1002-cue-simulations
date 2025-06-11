4347356a288e810bc6c66192dc7a89d4c6294bd9
Helen G Scott <helengracescott@gmail.com>
2025-06-11 10:30:09 -0400

    Shade regions based on exchange fluxes

--- mit1002_full/glc_vs_ace_timecourse/simulation/plot_results.py
+++ mit1002_full/glc_vs_ace_timecourse/simulation/plot_results.py
@@ -22,8 +22,22 @@ for filename in os.listdir(RESULTS_DIR):
         # Take "_results.pkl" off the end of the filename to get the experiment name
         c_source_name = filename.split("_results.pkl")[0]
 
+        # Find the cycles where the glucose exchange flux in negative
+        glucose_exchange_flux = experiment.fluxes_by_species[model_id]["EX_cpd00027_e0"]
+        glucose_cycles = glucose_exchange_flux[glucose_exchange_flux < 0].index
+
+        # Find the cycles where the acetate exchange flux is negative
+        acetate_exchange_flux = experiment.fluxes_by_species[model_id]["EX_cpd00029_e0"]
+        acetate_cycles = acetate_exchange_flux[acetate_exchange_flux < 0].index
+
         # Plot the biomass over time
         ax = experiment.total_biomass.plot(x="cycle")
+        # Shade in the cycles where glucose is being consumed
+        for cycle in glucose_cycles:
+            ax.axvspan(cycle - 0.5, cycle + 0.5, color="blue", alpha=0.1)
+        # Shade in the cycles where acetate is being consumed
+        for cycle in acetate_cycles:
+            ax.axvspan(cycle - 0.5, cycle + 0.5, color="orange", alpha=0.1)
         # Style the plot
         ax.set_ylabel("Biomass (g)")
         ax.set_title(f"MIT1002 Growth on {c_source_name}")
@@ -31,6 +45,12 @@ for filename in os.listdir(RESULTS_DIR):
 
         # Plot the growth rate over time
         ax = experiment.fluxes_by_species[model_id].plot(x="cycle", y=biomass_id)
+        # Shade in the cycles where glucose is being consumed
+        for cycle in glucose_cycles:
+            ax.axvspan(cycle - 0.5, cycle + 0.5, color="blue", alpha=0.1)
+        # Shade in the cycles where acetate is being consumed
+        for cycle in acetate_cycles:
+            ax.axvspan(cycle - 0.5, cycle + 0.5, color="orange", alpha=0.1)
         # Style the plot
         ax.set_ylabel("Growth Rate (1/h)")
         ax.set_title(f"MIT1002 Growth Rate on {c_source_name}")
@@ -43,4 +63,6 @@ for filename in os.listdir(RESULTS_DIR):
         # Style the plot
         ax.set_ylabel("Exchange Flux (mmol/gDW/h)")
         ax.set_title(f"MIT1002 Exchange Fluxes on {c_source_name}")
-        ax.figure.savefig(os.path.join(PLOTS_DIR, f"{c_source_name}_exchange_fluxes.png"))
+        ax.figure.savefig(
+            os.path.join(PLOTS_DIR, f"{c_source_name}_exchange_fluxes.png")
+        )
