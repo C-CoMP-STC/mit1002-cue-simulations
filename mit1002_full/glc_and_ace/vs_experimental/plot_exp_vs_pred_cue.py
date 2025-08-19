bb3198d5ffd19cf472a5074f2cc13cd1a4b41637
Helen G Scott <helengracescott@gmail.com>
2025-08-19 14:24:44 -0400

    Fix name order everywhere

--- mit1002_full/glc_and_ace/vs_experimental/plot_exp_vs_pred_cue.py
+++ mit1002_full/glc_and_ace/vs_experimental/plot_exp_vs_pred_cue.py
@@ -37,43 +37,43 @@ fba_data = pd.read_csv(
 bge_df.loc["FBA (O2=5)"] = {
     "Glucose Only": fba_data.loc["Glucose Only(O2 = 5)_fba", "bge"],
     "Acetate Only": fba_data.loc["Acetate Only(O2 = 5)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM": fba_data.loc["Glucose Heavy Mix(O2 = 5)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM (Early)": fba_data.loc[
+    "Glucose 8mM, Acetate 4mM": fba_data.loc["Glucose Heavy Mix(O2 = 5)_fba", "bge"],
+    "Acetate 8mM, Glucose 4mM (Early)": fba_data.loc[
         "Acetate Heavy Mix(O2 = 5)_fba", "bge"
     ],
 }
 bge_df.loc["FBA (O2=10)"] = {
     "Glucose Only": fba_data.loc["Glucose Only(O2 = 10)_fba", "bge"],
     "Acetate Only": fba_data.loc["Acetate Only(O2 = 10)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM": fba_data.loc["Glucose Heavy Mix(O2 = 10)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM (Early)": fba_data.loc[
+    "Glucose 8mM, Acetate 4mM": fba_data.loc["Glucose Heavy Mix(O2 = 10)_fba", "bge"],
+    "Acetate 8mM, Glucose 4mM (Early)": fba_data.loc[
         "Acetate Heavy Mix(O2 = 10)_fba", "bge"
     ],
 }
 bge_df.loc["FBA (O2=20)"] = {
     "Glucose Only": fba_data.loc["Glucose Only(O2 = 20)_fba", "bge"],
     "Acetate Only": fba_data.loc["Acetate Only(O2 = 20)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM": fba_data.loc["Glucose Heavy Mix(O2 = 20)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM (Early)": fba_data.loc[
+    "Glucose 8mM, Acetate 4mM": fba_data.loc["Glucose Heavy Mix(O2 = 20)_fba", "bge"],
+    "Acetate 8mM, Glucose 4mM (Early)": fba_data.loc[
         "Acetate Heavy Mix(O2 = 20)_fba", "bge"
     ],
 }
 bge_df.loc["FBA (O2=30)"] = {
     "Glucose Only": fba_data.loc["Glucose Only(O2 = 30)_fba", "bge"],
     "Acetate Only": fba_data.loc["Acetate Only(O2 = 30)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM": fba_data.loc["Glucose Heavy Mix(O2 = 30)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM (Early)": fba_data.loc[
+    "Glucose 8mM, Acetate 4mM": fba_data.loc["Glucose Heavy Mix(O2 = 30)_fba", "bge"],
+    "Acetate 8mM, Glucose 4mM (Early)": fba_data.loc[
         "Acetate Heavy Mix(O2 = 30)_fba", "bge"
     ],
 }
 bge_df.loc["FBA (O2=1000)"] = {
     "Glucose Only": fba_data.loc["Glucose Only(O2 = 1000)_fba", "bge"],
     "Acetate Only": fba_data.loc["Acetate Only(O2 = 1000)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM": fba_data.loc["Glucose Heavy Mix(O2 = 1000)_fba", "bge"],
-    "Glucose 4mM, Acetate 8mM (Early)": fba_data.loc[
+    "Glucose 8mM, Acetate 4mM": fba_data.loc["Glucose Heavy Mix(O2 = 1000)_fba", "bge"],
+    "Acetate 8mM, Glucose 4mM (Early)": fba_data.loc[
         "Acetate Heavy Mix(O2 = 1000)_fba", "bge"
     ],
-    "Glucose 4mM, Acetate 8mM (Late)": fba_data.loc[
+    "Acetate 8mM, Glucose 4mM (Late)": fba_data.loc[
         "Acetate Heavy Mix(O2 = 1000)_fba", "bge"
     ],
 }
